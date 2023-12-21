04c7f42ec5828d1bc1fb0aae3a3d137db8fdd132
Anas Khaled <anas.elfayoumy7@gmail.com>
2023-12-21 18:40:14 +0200

    Update import path for headerLinks constant

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -10,27 +10,27 @@ const Header = () => {
   return (
     <header className="border-b">
       <div className="wrapper flex-between">
-      <Logo />
+        <Logo />
 
-      <SignedIn>
-        <nav className="md:flex-between hidden w-full max-w-xs">
-          <NavItems />
-        </nav>
-      </SignedIn>
-
-      <div className="flex w-32 justify-end items-center gap-3">
         <SignedIn>
-          <UserButton afterSignOutUrl="/" />
-          <nav className="md:hidden">
-            <MobileNav />
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
           </nav>
         </SignedIn>
-        <SignedOut>
-          <Button asChild className="rounded-full" size="lg">
-            <Link href="/sign-in">Login</Link>
-          </Button>
-        </SignedOut>
-      </div>
+
+        <div className="flex w-32 justify-end items-center gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+            <nav className="md:hidden">
+              <MobileNav />
+            </nav>
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
       </div>
     </header>
   );
--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-import { headerLinks } from "../../constants";
+import { headerLinks } from "@/constants";
 
 const NavItems = () => {
   const pathname = usePathname();
